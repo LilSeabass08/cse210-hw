ed0a9a71914c982723cb6f66ac9721369260be54
Sebastian Ethington <sashi.ethington@gmail.com>
2025-05-08 09:24:00 -0600

    completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        List <int> num = new List<int>();
+        int input;
+        
+        do
+        {
+            Console.Write("Please enter a number (0 to stop). ");
+        input = int.Parse(Console.ReadLine());
+        
+        if (input != 0)
+            {
+                num.Add(input);
+            }
+        }
+        while (input != 0);
+
+        if (num.Count > 0)
+            {
+                int sum = num.Sum();
+                int average = (int)num.Average();
+                int max = num.Max();
+
+                Console.WriteLine($"Numbers entered: {string.Join(",", num)}");
+                Console.WriteLine($"Sum: {sum}");
+                Console.WriteLine($"Average {average}");
+                Console.WriteLine($"Max {max}");
+            }
+
     }
 }
